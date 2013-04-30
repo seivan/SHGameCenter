6d44c4677e9eda013a16e6fccc5e08a99d56e1ae
Seivan Heidari <seivan@kth.se>
2013-04-30 16:40:12 +0200

    Created private interfaces for the categories (to get rid of the warnings)

---
+++ Example/Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// BlocksKit
+#define COCOAPODS_POD_AVAILABLE_BlocksKit
+#define COCOAPODS_VERSION_MAJOR_BlocksKit 1
+#define COCOAPODS_VERSION_MINOR_BlocksKit 8
+#define COCOAPODS_VERSION_PATCH_BlocksKit 2
+
+// SHGameCenter
+#define COCOAPODS_POD_AVAILABLE_SHGameCenter
+#define COCOAPODS_VERSION_MAJOR_SHGameCenter 0
+#define COCOAPODS_VERSION_MINOR_SHGameCenter 2
+#define COCOAPODS_VERSION_PATCH_SHGameCenter 0
+
+// libffi
+#define COCOAPODS_POD_AVAILABLE_libffi
+#define COCOAPODS_VERSION_MAJOR_libffi 3
+#define COCOAPODS_VERSION_MINOR_libffi 0
+#define COCOAPODS_VERSION_PATCH_libffi 11
+
